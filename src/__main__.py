7afc4f234b110654aa580d9e63a7f7b23affb9fa
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-01-11 07:15:33 +0100

    using llamaindex for the llm

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,11 @@
 import os
 from pathlib import Path
-from langchain.llms import Ollama as LLM
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.callbacks.manager import CallbackManager
 from llama_index import VectorStoreIndex
+from llama_index.llms import Ollama as ModelLLM
+from llama_index.llms import ChatMessage
+from llama_index.embeddings import OllamaEmbedding as ModelEmbedding
 from llama_index import SimpleDirectoryReader as SDR
+from llama_index import ServiceContext, set_global_service_context
 import fire
 from .utils import setup_logging
 
@@ -14,14 +15,24 @@ PATH_INPUT = PATH_WORKDIR / "notes/notes"
 PATH_PROMPTS = PATH_WORKDIR / "represent/prompts"
 DEFAULT_NAME_PROMPT = "keywords"
 EXTENSION_TEXT = ".md"
+URL_BASE = "http://192.168.2.177:11434"
+MODEL = "mistral:instruct"
 
 log = setup_logging(DEBUG)
 
-llm = LLM(
-    base_url="http://192.168.2.177:11434",
-    model="mistral:instruct",
-    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+
+llm = ModelLLM(
+    base_url=URL_BASE,
+    model=MODEL
 )
+
+llm_embed = ModelEmbedding(
+    base_url=URL_BASE,
+    model_name=MODEL
+)
+
+service_context = ServiceContext.from_defaults(llm=llm, embed_model=llm_embed)
+set_global_service_context(service_context)
 
 
 def process_file(file_path, prompt):
@@ -29,9 +40,14 @@ def process_file(file_path, prompt):
     with open(file_path, 'r') as file:
         content = file.read()
         log(msg=f"file content is: {content}")
-        response = llm(f"{prompt}\n\n{content}")
+        messages = [
+            ChatMessage(role="system", content=prompt),
+            ChatMessage(role="user", content=f"heres the text: {content}")
+        ]
+
+        response = llm.chat(messages)
         log(msg=f"final response from llm: {response}")
-        return response
+        return response.message.content
 
 
 def represent(path_input: str = PATH_INPUT,
@@ -48,7 +64,7 @@ def represent(path_input: str = PATH_INPUT,
     log(msg=f"preparing output directory at {path_output_dir}")
     path_output_dir.mkdir(exist_ok=True)
 
-    # Process each file
+    log(msg=f"processing all files in {path_input}")
     for root, dirs, files in os.walk(path_input):
         log(msg=f"working on {root}")
         documents = []
