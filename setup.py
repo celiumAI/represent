24b30584246f6e5aafbf4dda96bab5a67f22a4fa
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-01-20 07:24:24 +0100

    checkpoint

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "represent=src:cli",
+            "represent=src.__main__:cli",
         ],
     },
     install_requires=[
--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
-from llama_index import VectorStoreIndex
-from llama_index.llms import Ollama as ModelLLM
-from llama_index.llms import ChatMessage
-from llama_index.embeddings import OllamaEmbedding as ModelEmbedding
+from llama_index import load_index_from_storage, VectorStoreIndex, StorageContext
+from langchain.chat_models.ollama import ChatOllama as ModelLLM
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.embeddings.ollama import OllamaEmbeddings as ModelEmbedding
 from llama_index import SimpleDirectoryReader as SDR
 from llama_index import ServiceContext, set_global_service_context
 import fire
@@ -11,7 +11,7 @@ from .utils import setup_logging
 
 DEBUG = True
 PATH_WORKDIR = Path("/home/mcfrank/")
-PATH_INPUT = PATH_WORKDIR / "notes/notes"
+PATH_INPUT = PATH_WORKDIR / "notes"
 PATH_PROMPTS = PATH_WORKDIR / "represent/prompts"
 DEFAULT_NAME_PROMPT = "keywords"
 EXTENSION_TEXT = ".md"
@@ -20,7 +20,6 @@ MODEL = "mistral:instruct"
 
 log = setup_logging(DEBUG)
 
-
 llm = ModelLLM(
     base_url=URL_BASE,
     model=MODEL
@@ -28,7 +27,7 @@ llm = ModelLLM(
 
 llm_embed = ModelEmbedding(
     base_url=URL_BASE,
-    model_name=MODEL
+    model=MODEL
 )
 
 service_context = ServiceContext.from_defaults(llm=llm, embed_model=llm_embed)
@@ -41,13 +40,13 @@ def process_file(file_path, prompt):
         content = file.read()
         log(msg=f"file content is: {content}")
         messages = [
-            ChatMessage(role="system", content=prompt),
-            ChatMessage(role="user", content=f"heres the text: {content}")
+            SystemMessage(content=prompt),
+            HumanMessage(content=f"heres the text: {content}")
         ]
 
-        response = llm.chat(messages)
+        response = llm(messages)
         log(msg=f"final response from llm: {response}")
-        return response.message.content
+        return response.content
 
 
 def represent(path_input: str = PATH_INPUT,
@@ -66,14 +65,17 @@ def represent(path_input: str = PATH_INPUT,
 
     log(msg=f"processing all files in {path_input}")
     for root, dirs, files in os.walk(path_input):
-        log(msg=f"working on {root}")
+        log(msg=f"working on dir {root}")
+        if ".git" in root or "index" in root:
+            log(msg=f"skipping dir: {root}")
+            continue
         documents = []
+        path_dir = Path(root)
         for file in files:
-            if file.endswith(".embeddings"):
+            if not file.endswith(".md"):
                 log(msg=f"skipping {file}")
                 continue
 
-            path_dir = Path(root)
             file_path = path_dir / file
             relative_path = file_path.relative_to(path_input)
             output_file_path = path_output_dir / relative_path
@@ -92,9 +94,24 @@ def represent(path_input: str = PATH_INPUT,
             with open(output_file_path, 'w') as output_file:
                 output_file.write(result)
 
-        index = VectorStoreIndex.from_documents(documents)
-        index.storage_context.persist(path_dir)
+        path_index = path_dir / "index"
+        if not os.path.exists(path_index):
+            index = VectorStoreIndex.from_documents(
+                documents
+            )
+            index.storage_context.persist(path_index)
+        else:
+            storage_context = StorageContext.from_defaults(
+                persist_dir=path_index
+            )
+            index = load_index_from_storage(storage_context=storage_context)
+            index.docstore.add_documents(documents)
+            index.storage_context.persist()
+
+
+def cli():
+    fire.Fire(represent)
 
 
 if __name__ == "__main__":
-    fire.Fire(represent)
+    cli()
